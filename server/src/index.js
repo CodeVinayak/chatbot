4e95a65569610aebea9a6cc38db1fee314761a29
Vinayak Singh <vinayaksingh10142@gmail.com>
2023-02-23 20:23:13 +0530

    removed commented line

--- server/src/index.js
+++ server/src/index.js
@@ -1,58 +1,3 @@
-// const express = require("express");
-// const OPENAI_API_KEY = "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";
-// const { Configuration, OpenAIApi } = require("openai");
-// const cors = require("cors");
-// const configuration = new Configuration({
-//   apiKey: OPENAI_API_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
-
-// const app = express();
-// app.use(cors());
-
-// app.use(express.json());
-
-// app.get("/ping", (req, res) => {
-//   res.json({
-//     message: "pong",
-//   });
-// });
-// app.post("/chat", (req, res) => {
-//   const question = req.body.question;
-
-//   openai
-//     .createCompletion({
-//       model: "text-davinci-003",
-//       prompt: question,
-//       max_tokens: 4000,
-//       temperature: 0,
-//     })
-//     .then((response) => {
-//       console.log({ response });
-//       return response?.data?.choices?.[0]?.text;
-//     })
-//     .then((answer) => {
-//       console.log({ answer });
-//       const array = answer
-//         ?.split("\n")
-//         .filter((value) => value)
-//         .map((value) => value.trim());
-
-//       return array;
-//     })
-//     .then((answer) => {
-//       res.json({
-//         answer: answer,
-//         propt: question,
-//       });
-//     });
-//   console.log({ question });
-// });
-
-// app.listen(3000, () => {
-//   console.log("Server is listening on port 3000");
-// });
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { Configuration, OpenAIApi } = require("openai");
 const express = require('express');
@@ -70,7 +15,7 @@ const openai = new OpenAIApi(configuration);
 // Enable CORS
 app.use(
   cors({
-    origin:"http://www.chatbot.vinayaksingh.com",
+    origin:"*",
     methods:["Get","POST","PUT","DELETE"]
 })
 );
